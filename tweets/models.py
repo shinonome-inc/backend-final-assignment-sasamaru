408c02c79f3f134c1d18e192b151c5cd70cd1feb
sasamaru-38 <h.sasaki.22.11.723@4nonome.com>
2023-04-26 15:28:41 +0900

    tweetsのコメントアウト

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,6 @@
+# from django.conf import settings
 # from django.db import models
 
 
 # class Tweet(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL)
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,9 @@
 # from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+# from .models import Tweet
+
 
 class HomeView(TemplateView):
+    # model = Tweet
     template_name = "tweets/home.html"
