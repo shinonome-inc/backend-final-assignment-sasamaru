04c1e1fae9736fbe607a3d31175f187960b056cc
sasamaru-38 <h.sasaki.22.11.723@4nonome.com>
2023-05-17 12:48:02 +0900

    デバッグツールバー追加

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,3 +23,10 @@ urlpatterns = [
     path("tweets/", include("tweets.urls")),
     path("", include("welcome.urls")),
 ]
+
+if settings.SQL_DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
